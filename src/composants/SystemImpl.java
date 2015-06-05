7b2361e6e01592385c0e33a9f480684f597273cc
fabien <fabien@pc4.home>
2015-06-05 13:23:31 +0200

    Enlever la boite du system si déposé dans nid

--- src/composants/SystemImpl.java
+++ src/composants/SystemImpl.java
@@ -319,12 +319,16 @@ public class SystemImpl extends SMA.System {
 					Case c = grille[agent.getPosition().getX()][agent
 							.getPosition().getY()];
 					if (!c.contientBoite()) {
-						c.addElement(boite);
-
-						if (agent.getCouleur().equals(boite.getCouleur())) {
-							energie = 2 * INITIAL_ENERGIE_AGENT / 3;
-						} else {
-							energie = INITIAL_ENERGIE_AGENT / 3;
+						
+						if(c.getNid() != null){
+							if (agent.getCouleur().equals(boite.getCouleur())) {
+								energie = 2 * INITIAL_ENERGIE_AGENT / 3;
+							} else {
+								energie = INITIAL_ENERGIE_AGENT / 3;
+							}
+						}
+						else{
+							c.addElement(boite);
 						}
 					}
 				}
